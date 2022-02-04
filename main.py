d778cc95cc9fb4ae69f639ead0e83a72f7cf0283
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2022-02-05 00:00:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageDraw, ImageFont
 
 image = Image.new('RGB', (1000, 900), (255, 255, 255))
 draw = ImageDraw.Draw(image)
-
 import random
 import os
 import datetime
@@ -43,4 +42,83 @@ if st.button("CREATE"):
         bar.progress(iw+1)
         time.sleep(0.1)
 
-    
+    company = message
+    color = 'rgb(0, 0, 0)'
+    #font = ImageFont.truetype('arial.ttf', size=80)
+    draw.text((x, y), message, fill=color)
+
+    # adding an unique id number. You can manually take it from user
+    (x, y) = (600, 75)
+    idno = random.randint(10000000, 90000000)
+    message = str('ID ' + str(idno))
+    color = 'rgb(0, 0, 0)'  # black color
+    #font = ImageFont.truetype('arial.ttf', size=60)
+    draw.text((x, y), message, fill=color)
+
+    (x, y) = (50, 250)
+    name = message2
+    if(name=='Male'):
+        message2='Male'
+        color = 'rgb(0, 0, 0)'  # black color
+        #font = ImageFont.truetype('arial.ttf', size=45)
+        draw.text((x, y), message2, fill=color)
+
+        (x, y) = (50, 350)
+    else:
+        message2='Female'
+        color = 'rgb(0, 0, 0)'  # black color
+        #font = ImageFont.truetype('arial.ttf', size=45)
+        draw.text((x, y), message2, fill=color)
+
+        (x, y) = (50, 350)
+
+    color = 'rgb(0, 0, 0)'  # black color
+    draw.text((x, y), message3, fill=color)
+    (x, y) = (250, 350)
+    color = 'rgb(0, 0, 0)'  # black color
+    draw.text((x, y), message4, fill=color)
+
+    (x, y) = (50, 450)
+
+    color = 'rgb(0, 0, 0)'  # black color
+    draw.text((x, y), message5, fill=color)
+
+    (x, y) = (50, 550)
+
+    color = 'rgb(255, 0, 0)'  # black color
+    draw.text((x, y), message6, fill=color)
+
+    (x, y) = (50, 650)
+
+    color = 'rgb(0, 0, 0)'  # black color
+    draw.text((x, y), message7, fill=color)
+
+    (x, y) = (50, 750)
+
+    color = 'rgb(0, 0, 0)'  # black color
+    draw.text((x, y), message8, fill=color)
+
+    #NEXT
+    image.save(str(name) + '.png')
+
+    img = qrcode.make(str(message)+'\n'+str(message2)+'\n'+str(message3)+'\n'+str(message4)+'\n'+str(message5)+'\n'+str(message6)+'\n'+str(message7)+'\n'+str(message8))  # this info. is added in QR code, also add other things
+    img.save(str(idno) + '.png')
+
+    til = Image.open(name + '.png')
+    im = Image.open(str(idno) + '.png')  # 25x25
+    til.paste(im, (600, 350))
+    til.save(name + '.png')
+    #a=st.write(f'{name}.png')
+    #imgh=Image.open(f'{name}.png')
+    #st.image(imgh,width=200)
+    image = Image.open(f'{name}.png')
+    col1, col2 = st.columns([0.1, 0.2])
+    with col1:
+        st.markdown("""<style> font{font-size:5px; font-family:'Cooper Black';color:#FF9633;<style><p>Result</p>""",
+                    unsafe_allow_html=True)
+    with col2:
+        st.image(image, width=250)
+    # print(('\n\n\nYour ID Card Successfully created in a PNG file ' + name + '.png'))
+
+# save the edited image
+
